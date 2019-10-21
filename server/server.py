1b4679c8c8a7cbe7091b63b843dc4e1c6129f1e0
yssong <thddudtn01@gmail.com>
2019-10-21 22:28:50 +0900

    server updated

--- server/server.py
+++ server/server.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 from werkzeug import secure_filename
 from flask_restful import reqparse, abort, Api, Resource
+import json
+from pprint import pprint
+
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/fileUpload', methods = ['GET', 'POST'])
+@app.route('/UserEnroll', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
-      #result = request.get_json
-      #print(jsonify(result))
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
+      raw_data = request.json
+      number = raw_data["number"]
+      data = json.dumps(raw_data, encoding='utf-8')
+
+      file = open(number+".json", 'w')
+      file.write(data)
+      data = json.loads(data)
+      file.close()
       return 'uploads directory -> file upload success!'
+  
+"""   list_file = open('./client_list/list.json', 'r').read()
+   client_list = json.loads(list_file)
+   try:
+      if not client_list[number]:
+         return 'This number is already enrolled'
+   except:
+      list_file = open('./client_list/list.json', 'w')
+      client_list[number].append("0")
+      data = json.dumps(client_list, encoding='utf-8')
+      #list_file.write(data_)
+   return 'uploads directory -> file upload success!'
+"""
 
 
 if __name__ == '__main__':
