516a6ce96b1cfb16c573fe4000a17b029f38e706
yssong <thddudtn01@gmail.com>
2019-12-07 21:38:27 +0900

    [package] path error fixed

--- packaging.sh
+++ packaging.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp -r server/ ./icu_1.0/usr/lib/python2.7/dist-packages/icu_1.0/server
-cp -r server/ ./icu_1.0/usr/lib/python2.7/dist-packages/icu_1.0/ML
+cp -r server/ ./icu_1.0/usr/lib/python2.7/dist-packages/icu/server
+cp -r server/ ./icu_1.0/usr/lib/python2.7/dist-packages/icu/ML
 
 dpkg -b icu_1.0
 
-rm -r ./icu_1.0/usr/lib/python2.7/dist-packages/icu_1.0/server
-rm -r ./icu_1.0/usr/lib/python2.7/dist-packages/icu_1.0/ML
+rm -r ./icu_1.0/usr/lib/python2.7/dist-packages/icu/server
+rm -r ./icu_1.0/usr/lib/python2.7/dist-packages/icu/ML
